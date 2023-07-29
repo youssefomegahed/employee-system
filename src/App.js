1e3bea0828d7f95ac952a8e414251c60a6bd05a3
Youssef Megahed <youssefosama@aucegypt.edu>
2023-07-29 21:11:09 +0300

    created a basic login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import React, { useState } from "react";
+import "./App.css";
+
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    if (username && password) {
+      alert("Logged in!");
+    } else {
+      alert("Please enter username and password!");
+    }
+  };
+
   return (
-    <div>
-      <h1>App</h1>
+    <div className="App">
+      <h1>Login</h1>
+      <div>
+        <label>Username:</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button onClick={handleLogin}>Login</button>
     </div>
   );
 }
