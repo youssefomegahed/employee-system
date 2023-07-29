eb54ad9cff57d2f8252fe001786d6adc95455f4d
Youssef Megahed <youssefosama@aucegypt.edu>
2023-07-29 21:36:24 +0300

    added bootstrap for styling

--- src/App.js
+++ src/App.js
@@ -4,35 +4,42 @@ import "./App.css";
 function App() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleLogin = () => {
     if (username && password) {
-      alert("Logged in!");
+      alert("Login successful!");
+      setErrorMessage("");
     } else {
-      alert("Please enter username and password!");
+      setErrorMessage("Please enter a valid username and password.");
     }
   };
 
   return (
-    <div className="App">
+    <div className="container">
       <h1>Login</h1>
-      <div>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+      <div className="form-group">
         <label>Username:</label>
         <input
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
+          className="form-control"
         />
       </div>
-      <div>
+      <div className="form-group">
         <label>Password:</label>
         <input
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          className="form-control"
         />
       </div>
-      <button onClick={handleLogin}>Login</button>
+      <button className="btn-login" onClick={handleLogin}>
+        Login
+      </button>
     </div>
   );
 }
